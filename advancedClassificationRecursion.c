64c82d68e7442ea4bcccad3b808a6ab50e72a632
Shani Vahav <92265738+ShaniVahav@users.noreply.github.com>
2021-11-04 16:23:22 +0200

    Add files via upload

---
+++ advancedClassificationRecursion.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+int sumOfDigit(int num){
+    int sum = 0;
+ while(num > 0){       
+        sum++;
+        num = num/10; 
+    }
+    return sum;
+}
+
+int armstrongRecursion(int num, int sumOfDig){
+int dig, sum = 0;
+
+  if (num > 0){
+     dig = num % 10;
+     sum = pow(dig, sumOfDig) + armstrongRecursion (num/10, sumOfDig);
+    }
+    return sum;
+}
+
+int isArmstrong (int num){
+    int sum;
+    int sumOfDig = sumOfDigit(num);
+    sum = armstrongRecursion(num, sumOfDig);
+if (num == sum) return 1;
+else return 0;
+}
+
+
+int isPalindrome(int num){
+     if(num < 10) return 1; 
+    int first = 0, numOfDigit = (int)log10(num) ;
+    int last = num%10 ;
+    first = (int) (num/pow(10,numOfDigit));
+    if(first != last) return 0;
+    else{ 
+        num = num%((int)pow(10,numOfDigit)); //remove first
+      num = num/10; //remove last
+      return (isPalindrome(num));
+    }
+}
